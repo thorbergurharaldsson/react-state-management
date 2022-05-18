064c44af9378c2f57e498a7a2d5bce177ebef776
thorbergurharaldsson <thorbergur@haraldssonprod.is>
2022-05-18 13:58:54 +0000

    Created a theme state for dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Layout from "./components/Layout";
+import { Main } from "./components/Main";
+import ThemeContext from "./contexts/ThemeContext";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const value = { theme, setTheme };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeContext.Provider value={value}>
+        <Layout>
+          <Main />
+        </Layout>
+      </ThemeContext.Provider>
     </div>
   );
 }
---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,8 @@
+import React, { createContext } from "react";
+
+const ThemeContext = createContext({
+  theme: "light",
+  setTheme: () => {},
+});
+
+export default ThemeContext;
